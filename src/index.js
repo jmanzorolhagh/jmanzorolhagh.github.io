a263499e01ab7dee8e4623e5de9ed4563accbf68
joshua manzorolhagh <joshuamanzorolhagh@gmail.com>
2022-02-10 17:53:49 -0500

    localized font for load optimization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import './index.scss';
+import './fonts/Rowdies/Rowdies-Regular.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
