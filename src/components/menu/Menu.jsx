ca50fb8fd07e58ebe8e4af1085844187b620de56
Joshua <joshuamanzorolhagh@gmail.com>
2025-11-04 18:21:01 -0500

    Updated colors, fonts, and background overlay

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,16 +4,17 @@ export default function Menu({open, setOpen}) {
   return <div className={"menu " + (open && 'active')}>
       <ul>
           <li onClick={() => setOpen(false)}>
-              <a href="#intro">intro</a>
+              <a href="#intro">Intro</a>
           </li>
           <li onClick={() => setOpen(false)}>
-              <a href="#works">portfolio</a>
+              <a href="#works">Portfolio</a>
           </li>
           <li onClick={() => setOpen(false)}>
-              <a href="#contact">contact</a>
+              <a href="#skills">Skills</a>
           </li>
-
+          <li onClick={() => setOpen(false)}>
+              <a href="#contact">Contact</a>
+          </li>
       </ul>
-
   </div>;
 }
