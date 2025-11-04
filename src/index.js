b5b093ef0060c3212a7b5b41631c0719a043e5d1
Joshua <joshuamanzorolhagh@gmail.com>
2025-11-04 18:18:26 -0500

    Updated to reflect new React design practices

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.scss';
-import './fonts/Rowdies/Rowdies-Regular.ttf';
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+
+
+const root = createRoot(container);
+
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
