4dc641621c45f5006a41136bcf9764943861d24a
Joshua <joshuamanzorolhagh@gmail.com>
2025-11-04 18:18:02 -0500

    Introduced new skills component and global button design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Works from "./components/works/Works";
+import Skills from "./components/skills/Skills"; // 1. IMPORTED
 import Contact from "./components/contact/Contact";
 import "./App.scss";
 import {useState} from "react";
 import Menu from "./components/menu/Menu";
+
 function App() {
   const [open, setOpen] = useState(false);
   return (
@@ -14,6 +16,7 @@ function App() {
       <div className="sections">
         <Intro/>
         <Works/>
+        <Skills/>
         <Contact/>
       </div>
     </div>
